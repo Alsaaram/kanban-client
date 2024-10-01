5797f19c15cd945e33dee37d9e34918da0927c0e
Alsaaram <131556435+Alsaaram@users.noreply.github.com>
2024-09-30 21:10:39 -0400

    Login page display update

--- src/routes/_unauthenticated/auth/login.tsx
+++ src/routes/_unauthenticated/auth/login.tsx
@@ -6,7 +6,17 @@ export const Route = createFileRoute("/_unauthenticated/auth/login")({
 
 function LoginPage() {
   return (
-    <div className="flex flex-col absolute justify-center top-0 w-full mt-0 mb-12"></div>
+    <div className="flex flex-col justify-center my-[50px] min-h-[calc(100vh-100px)] ring">
+      <section className="flex flex-col my-0 mx-auto w-[400px] py-8 px-10 shadown-2xl rounded-[3px] border-box ring">
+        <div className="flex flex-col content-center items-center text-center mb-4 ring">
+          <span className="inline-block relative leading-none select-none whitespace-normal h-10 ring">
+            <div className="h-full fill-inherit text-3xl font-extrabold">Kobah</div>
+          </span>
+          <div className="flex flex-col pt-6 content-center items-center font-medium text-lg">
+            <h5>Log in to continue</h5>
+          </div>
+        </div>
+      </section>
+    </div>
   );
-};
-
+}
